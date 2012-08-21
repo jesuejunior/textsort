f98e472e81fc9b955106e9c0e20b4fbfe37d504b
JesueJunior <jesuesousa@gmail.com>
2012-08-21 00:55:13 -0300

    Finalizando InsertionSort

--- src/sort/InsertionSort.java
+++ src/sort/InsertionSort.java
@@ -4,60 +4,18 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class InsertionSort {
-	private int[]data;
-	private static final Random generator=new Random();
 	
-	public InsertionSort(int size){
-		data=new int[size];
-		
-		//preenche o array com inteiros aleatorios entre 10 e 99
-		for(int i=0; i<size; i++)
-			data[i]=10 + generator.nextInt(90);
-	}
-	
-	public void sort()
-	{
-		int insert;
-		for(int next=1; next<data.length;next++)
-		{
-			//armazena o valor no elemento atual
-			insert=data[next];
-		
-			int moveItem=next;
-			
-			while(moveItem>0 && data[moveItem-1]>insert)
-			{
-				data[moveItem]=data[moveItem-1];
-				moveItem--;
-			}
-			data[moveItem]=insert; //posiciona o elemento inserido
-			printPass(next, moveItem); //passagem de aaida do algoritmo
-		}
-	}
-	
-	public void printPass(int pass, int index)
-	{
-		System.out.println(String.format("depois da passagem %2d: ", pass));
-		
-		//gera saida dos elementos ate o item trocado
-		for(int i=0;i<index;i++)
-			System.out.print(data[i]+" ");
-		
-		System.out.print(data[index]+"*"); //indica troca
-		
-		//termina de gerar a saida do array
-		for (int i= index+1; i<data.length;i++)
-			System.out.print(data[i]+" ");
-		
-		System.out.print("\n");
-		
-		for (int i=0; i<=pass; i++)
-			System.out.print("-- ");
-		System.out.println("\n");		
-	}
-	
-	public String toString()
-	{
-		return Arrays.toString(data);
-	}	
+	public String[] insertionSort(String[] list){
+        String temp="";
+        for(int i=0;i<list.length;i++){
+            for(int j=i+1;j<list.length;j++){
+               if(list[i].compareToIgnoreCase(list[j])>0){
+                   temp = list[i];
+                   list[i]=list[j];
+                   list[j]=temp;
+               }
+            }
+        }
+        return list;
+    }
 }
--- src/start/InsertionSortStart.java
+++ src/start/InsertionSortStart.java
@@ -1,19 +1,37 @@
 package start;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import sort.InsertionSort;
+import utils.DoFile;
 
 public class InsertionSortStart {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		InsertionSort sortArray=new InsertionSort(10);
-		
-		System.out.println("Array não ordenado: ");
-		System.out.println(sortArray+"\n");
-		
-		sortArray.sort();
-		
-		System.out.println("Array ordenado");
-		System.out.println(sortArray);
-	}
+		SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+		String[] lista = DoFile.read("arquivo.txt");
 
+		InsertionSort sort = new InsertionSort();
+		long start = System.currentTimeMillis();
+		Date now = new Date(start);
+		System.out.println("Inicio: " + format.format(now));
+		String[] fileSorted = sort.insertionSort(lista);
+
+		System.out.println("QTDE de palavras: " + fileSorted.length);
+		long end = System.currentTimeMillis();
+		now = new Date(end);
+		System.out.println("Fim: " + format.format(now));
+		System.out.println("Tempo gasto em segundos: " + ((end - start) / 1000)
+				+ " s");
+		System.out.println("Tempo gasto em milisegundos: " + (end - start)
+				+ " ms");
+
+		for (int i = 0; i < fileSorted.length; i++) {
+
+			DoFile.save("out_InsertionSort.txt", fileSorted[i] + "\n", true);
+		}
+	}
 
 }
