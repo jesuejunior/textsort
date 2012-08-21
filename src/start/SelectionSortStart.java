5fdb6a2e5ec0bc805c593d6ac29d1a43b7c8d44d
JesueJunior <jesuesousa@gmail.com>
2012-08-21 00:55:38 -0300

    Finalizando SelectionSort

--- src/start/SelectionSortStart.java
+++ src/start/SelectionSortStart.java
@@ -1,23 +1,35 @@
 package start;
-import sort.SelectionSort;
 
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import sort.SelectionSort;
+import utils.DoFile;
 
 public class SelectionSortStart {
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
-		SelectionSort sortArray=new SelectionSort(10);
-		
-		System.out.println("Array nao ordenado: ");
-		System.out.println(sortArray+"\n");
-		
-		sortArray.sort();
-		
-		System.out.println("Array ordenado");
-		System.out.println(sortArray);
+		SimpleDateFormat format = new SimpleDateFormat("HH:mm:ss");
+		String[] lista = DoFile.read("arquivo.txt");
+
+		SelectionSort sort = new SelectionSort();
+		long start = System.currentTimeMillis();
+		Date now = new Date(start);
+		System.out.println("Inicio: " + format.format(now));
+		String[] fileSorted = sort.selectionSort(lista);
+
+		System.out.println("QTDE de palavras: " + fileSorted.length);
+		long end = System.currentTimeMillis();
+		now = new Date(end);
+		System.out.println("Fim: " + format.format(now));
+		System.out.println("Tempo gasto em segundos: " + ((end - start) / 1000)	+ " s");
+		System.out.println("Tempo gasto em milisegundos: " + (end - start) + " ms");
+		for (int i = 0; i < fileSorted.length; i++) {
+			
+			DoFile.save("out.txt", fileSorted[i] + "\n", true);
+		}
 	}
 
 }
