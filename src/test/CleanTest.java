f838f93ebfc3776312ae3f22430deaf3697b906e
JesueJunior <jesuesousa@gmail.com>
2012-08-21 00:56:32 -0300

    Correção de testes

--- src/test/CleanTest.java
+++ src/test/CleanTest.java
@@ -1,11 +1,8 @@
 package test;
 
 import static org.junit.Assert.*;
-
 import java.util.ArrayList;
-
 import org.junit.Test;
-
 import utils.Helper;
 
 public class CleanTest {
@@ -16,8 +13,7 @@ public class CleanTest {
 		ArrayList<String> resultadoEsperado = new ArrayList<String>();
 		resultadoEsperado.add("jesue");
 		resultadoEsperado.add("junior");
-				
-		
+
 		assertEquals("@", resultadoEsperado, h.clean("jesue@junior"));
 		assertEquals("!", resultadoEsperado, h.clean("jesue!junior"));
 		assertEquals("#", resultadoEsperado, h.clean("jesue#junior"));
@@ -28,40 +24,40 @@ public class CleanTest {
 		assertEquals("=", resultadoEsperado, h.clean("jesue=junior"));
 		assertEquals("\"", resultadoEsperado, h.clean("jesue\"junior"));
 		assertEquals(resultadoEsperado, h.clean("jesue junior"));
-			
+
 	}
-	
+
 	@Test
 	public void retiraAcentos() {
 		Helper h = new Helper();
 		ArrayList<String> resultadoEsperado = new ArrayList<String>();
 		resultadoEsperado.add("jesue");
 		resultadoEsperado.add("junior");
-		
+
 		ArrayList<String> esperado = new ArrayList<String>();
 		esperado.add("aeiou");
 		esperado.add("aeiou");
 		esperado.add("aoc");
-		
+
 		ArrayList<String> acentos = new ArrayList<String>();
 		acentos.addAll(h.clean("àèìòù"));
 		acentos.addAll(h.clean("áéíóú"));
 		acentos.addAll(h.clean("ãõç"));
-		
+
 		assertEquals(esperado, acentos);
-			
-	}	
-	
+
+	}
+
 	@Test
 	public void retiraEmBranco() {
 		Helper h = new Helper();
 		ArrayList<String> resultadoEsperado = new ArrayList<String>();
 		resultadoEsperado.add("jesue");
 		resultadoEsperado.add("junior");
-		
+
 		assertEquals(resultadoEsperado, h.clean("jesue\"junior#"));
 		assertEquals(resultadoEsperado, h.clean("jesue\"junior#  "));
 		assertEquals(resultadoEsperado, h.clean("jesue\"junior#  $  "));
-	}	
+	}
 
 }
